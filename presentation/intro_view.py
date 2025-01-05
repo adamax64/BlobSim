25ee507cd9cb983fb0ebe828234941656895425e
adammakai8 <h879389@stud.u-szeged.hu>
2025-01-05 11:49:46 +0100

    fix blob-league relation warning message (visible only in debug mode)

--- presentation/intro_view.py
+++ presentation/intro_view.py
@@ -37,7 +37,7 @@ def show_intro_view():
     title_padding = ' ' * _get_padding_size(width, 24)
     print(f'{title_padding}BLOB CHAMPIONSHIP SYSTEM')
     version_padding = ' ' * _get_padding_size(width, 6)
-    print(f'{version_padding}v1.1.1')
+    print(f'{version_padding}v1.1.2')
     copyright_padding = ' ' * _get_padding_size(width, 22)
     print(f'{copyright_padding}by Adamax-works © 2025')
     print()
