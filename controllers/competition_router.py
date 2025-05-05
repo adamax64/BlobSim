2a3c7ca9dd35e437fbcdc16d07fc5b0add80ee7e
adammakai8 <h879389@stud.u-szeged.hu>
2025-05-05 19:41:46 +0200

    #23 Add save action endpoint

--- controllers/competition_router.py
+++ controllers/competition_router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException
-from domain.competition_service import load_competition_data
+from domain.competition_service import load_competition_data, create_action as service_create_action
 from domain.dtos.event_dto import EventDto
+from domain.dtos.save_action_dto import SaveActionDto
 from domain.exceptions.no_current_event_exception import NoCurrentEventException
 
 router = APIRouter(prefix="/competition", tags=["competition"])
@@ -13,6 +14,15 @@ async def get_current_event() -> EventDto:
         return event_data
     except NoCurrentEventException:
         raise HTTPException(status_code=400, detail="NO_CURRENT_EVENT")
+    except Exception as e:
+        print(e.with_traceback(None))
+        raise HTTPException(status_code=500, detail=f"{e.with_traceback(None)}")
+
+
+@router.post("/action")
+async def create_action(action: SaveActionDto) -> None:
+    try:
+        service_create_action(action)
     except Exception as e:
         print(e.with_traceback(None))
         raise HTTPException(status_code=500, detail=f"{e.with_traceback(None)}")
