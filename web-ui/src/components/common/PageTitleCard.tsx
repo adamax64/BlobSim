3e0fe730e4bbabb946da0d2c14aceb59f16c4a76
adammakai8 <h879389@stud.u-szeged.hu>
2025-05-10 09:52:54 +0200

    #23 Add generic event page and route
    * load event data calling the current event endpoint
    * extended PageTitleCard component with skeleton component to show loading placeholder when the title is not yet available

--- web-ui/src/components/common/PageTitleCard.tsx
+++ web-ui/src/components/common/PageTitleCard.tsx
@@ -1,23 +1,30 @@
-import { Box, Card, CardHeader } from "@mui/material";
-import { BlobIcon } from "../icons/BlobIcon";
-import { useMemo } from "react";
+import { Box, Card, CardHeader, Skeleton } from '@mui/material';
+import { BlobIcon } from '../icons/BlobIcon';
+import { useMemo } from 'react';
 
 interface PageTitleCardProps {
-  title: string;
+  title?: string;
   blobIconColor?: string;
   center?: boolean;
 }
 
 export function PageTitleCard({ title, blobIconColor, center }: PageTitleCardProps) {
-  const iconColor = useMemo(() => blobIconColor ?? `#${Math.floor(Math.random() * 16777215).toString(16)}`, [blobIconColor])
+  const iconColor = useMemo(
+    () => blobIconColor ?? `#${Math.floor(Math.random() * 16777215).toString(16)}`,
+    [blobIconColor],
+  );
 
-  return <Card>
-    <CardHeader title={
-      <Box display="flex" gap={2} justifyContent={center ? "space-between" : "start"}>
-        <BlobIcon size={32} color={iconColor} />
-        {title}
-        <BlobIcon size={32} color={iconColor} />
-      </Box>
-    } />
-  </Card>
+  return (
+    <Card>
+      <CardHeader
+        title={
+          <Box display="flex" gap={2} justifyContent={center ? 'space-between' : 'start'}>
+            <BlobIcon size={32} color={iconColor} />
+            {title || <Skeleton variant="text" width={320} />}
+            <BlobIcon size={32} color={iconColor} />
+          </Box>
+        }
+      />
+    </Card>
+  );
 }
