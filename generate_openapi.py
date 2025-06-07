d829285f1fd02c64c42fb3fdd08aff9b3eb3614f
Adam Makai <adam.makai@tngtech.com>
2025-06-07 11:19:27 +0200

    Fix generate openapi script to use it independent of machine

--- generate_openapi.py
+++ generate_openapi.py
@@ -1,15 +1,20 @@
-import json
 from pathlib import Path
-from main import app  # Import the FastAPI app from main.py
+from fastapi.openapi.utils import get_openapi
+from main import app
 
 
-def generate_openapi_json(output_path: str):
-    openapi_schema = app.openapi()
-    with open(output_path, "w", encoding="utf-8") as f:
-        json.dump(openapi_schema, f, indent=2)
-    print(f"OpenAPI JSON has been written to {output_path}")
+def generate_openapi_json(output_file: Path):
+    with open(output_file, "w") as f:
+        import json
+        json.dump(get_openapi(
+            title=app.title,
+            version=app.version,
+            openapi_version=app.openapi_version,
+            description=app.description,
+            routes=app.routes,
+        ), f, indent=2)
 
 
 if __name__ == "__main__":
-    output_file = Path("d:/SoftwareDevelopement/BlobSim/web-ui/openapi.json")
+    output_file = Path("web-ui/openapi.json")
     generate_openapi_json(output_file)
