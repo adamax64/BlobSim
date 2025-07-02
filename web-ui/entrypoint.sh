b6d545b5e32c93c95f502e964669889a4b20ebb5
adammakai8 <h879389@stud.u-szeged.hu>
2025-07-02 15:07:07 +0200

    Add entrypoint script for Nginx to generate environment variables

---
+++ web-ui/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Generate env.js from environment variables
+cat <<EOF > /usr/share/nginx/html/env.js
+window.env = {
+  VITE_API_BASE_URL: "${VITE_API_BASE_URL}"
+};
+EOF
+
+# Start nginx
+nginx -g "daemon off;"
