75d467c12cfeec541dc91bc947ba54bbaaebf2a7
adammakai8 <h879389@stud.u-szeged.hu>
2025-09-14 19:05:26 +0200

    42 - add news repository, dto and services

---
+++ domain/news_services/news_service.py
@@ -0,0 +1,97 @@
+from sqlalchemy.orm import Session
+
+from data.db.db_engine import transactional
+from data.model.event_type import EventType
+from data.model.news import News
+from data.model.news_type import NewsTypeDbo
+from data.persistence.news_repository import delete_news_with_type, delete_old_news, save_news
+from domain.sim_data_service import get_sim_time
+
+
+@transactional
+def add_blob_created_news(blob_name: str, session: Session):
+    _create_news(NewsTypeDbo.BLOB_CREATED, [blob_name], session)
+    delete_news_with_type(NewsTypeDbo.BLOB_IN_CREATION, session)
+
+
+@transactional
+def add_blob_in_creation_news(session: Session):
+    _create_news(NewsTypeDbo.BLOB_IN_CREATION, [], session)
+
+
+@transactional
+def add_blob_terminated_news(blob_name: str, session: Session):
+    _create_news(NewsTypeDbo.BLOB_TERMINATED, [blob_name], session)
+
+
+@transactional
+def add_event_starting_news(league_name: str, round: int, event_type: EventType, session: Session):
+    _create_news(NewsTypeDbo.EVENT_STARTED, [league_name, str(round), str(event_type)], session)
+
+
+@transactional
+def add_ongoing_event_news(league_name: str, round: int, event_type: EventType, session: Session):
+    _create_news(NewsTypeDbo.ONGOING_EVENT, [league_name, str(round), str(event_type)], session)
+    delete_news_with_type(NewsTypeDbo.EVENT_STARTED, session)
+
+
+@transactional
+def add_event_ended_news(league_name: str, round: int, first: str, second: str, third: str, session: Session):
+    _create_news(NewsTypeDbo.EVENT_ENDED, [league_name, str(round), first, second, third], session)
+    delete_news_with_type(NewsTypeDbo.ONGOING_EVENT, session)
+
+
+@transactional
+def add_season_ended_news(league_name: str, winner: str, session: Session):
+    _create_news(NewsTypeDbo.SEASON_ENDED, [league_name, winner], session)
+
+
+@transactional
+def add_rookie_of_the_year_news(winner: str, session: Session):
+    _create_news(NewsTypeDbo.ROOKIE_OF_THE_YEAR, [winner], session)
+
+
+@transactional
+def add_new_season_news(season: int, transfers: dict[str, list[str]], retired: list[str], rookies: list[str], session: Session):
+    """
+    Creates a news record for the new starting season with the following data:
+    - season: the number of the newly starting season
+    - transfers: the list of leagues and the blobs transferred to the given season
+    - retired: blobs that are retired from the championship system
+    - rookies: new rookies that are starting their carreers
+    """
+    flattened_transfers_array: list[str] = []
+    for (league, blobs) in transfers.items():
+        flattened_transfers_array.append(league)
+        flattened_transfers_array.append(str(len(blobs)))
+        for blob in blobs:
+            flattened_transfers_array.append(blob)
+
+    _create_news(
+        NewsTypeDbo.NEW_SEASON,
+        [
+            str(season),
+            str(len(transfers)),
+            *flattened_transfers_array,
+            str(len(retired)),
+            *retired,
+            str(len(rookies)),
+            *rookies
+        ],
+        session
+    )
+    delete_old_news(session)
+
+
+@transactional
+def add_new_grandmaster_news(grandmaster: str, session: Session):
+    _create_news(NewsTypeDbo.NEW_GRANDMASTER, [grandmaster], session)
+
+
+def _create_news(news_type: NewsTypeDbo, data: list[str], session: Session):
+    sim_date = get_sim_time(session)
+    save_news(News(
+        date=sim_date,
+        news_type=news_type,
+        news_data=data
+    ), session)
