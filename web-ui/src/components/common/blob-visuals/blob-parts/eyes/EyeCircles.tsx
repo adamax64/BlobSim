a223f069d4a51ed5a1a495edf0a2b5e084fa7dd3
adammakai8 <h879389@stud.u-szeged.hu>
2026-02-21 12:38:59 +0100

    64 - wip add eyecircle component

---
+++ web-ui/src/components/common/blob-visuals/blob-parts/eyes/EyeCircles.tsx
@@ -0,0 +1,16 @@
+export const EyeCircles = () => {
+  return (
+    <>
+      <path
+        fill="none"
+        stroke="#000000"
+        strokeWidth={1}
+        strokeLinecap="butt"
+        strokeLinejoin="miter"
+        strokeOpacity={1}
+        d="m 40,42 a 10 35 180 0 0 10 0"
+        id="eye-circle"
+      />
+    </>
+  );
+};
